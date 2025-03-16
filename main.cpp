981f2fa070f33d3e0271f34521f4ed2fc311b628
Ngocduc01 <ducstd1@gmail.com>
2025-03-16 16:10:15 +0700

    Update main.cpp
    sửa lại kích thước của nhân vật

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 450;
-const int DINO_WIDTH = 80;
-const int DINO_HEIGHT = 80;
+const int DINO_WIDTH = 100;   // Đã sửa từ 80 thành 100
+const int DINO_HEIGHT = 100;  // Đã sửa từ 80 thành 100
 const int OBSTACLE_WIDTH = 40;
 const int OBSTACLE_HEIGHT = 80;
 const int FLYING_OBSTACLE_WIDTH = 80;
@@ -106,7 +106,7 @@ public:
 
     Dino(SDL_Texture* tex1, SDL_Texture* tex2, SDL_Texture* jumpTex) {
         x = 50;
-        y = SCREEN_HEIGHT - DINO_HEIGHT;
+        y = SCREEN_HEIGHT - DINO_HEIGHT; // DINO_HEIGHT giờ là 100
         velocity = 0;
         isJumping = false;
         jumpCount = 0;
@@ -122,7 +122,7 @@ public:
 
     void reset(SDL_Texture* tex1, SDL_Texture* tex2, SDL_Texture* jumpTex) {
         x = 50;
-        y = SCREEN_HEIGHT - DINO_HEIGHT;
+        y = SCREEN_HEIGHT - DINO_HEIGHT; // DINO_HEIGHT giờ là 100
         velocity = 0;
         isJumping = false;
         jumpCount = 0;
@@ -146,7 +146,7 @@ public:
         velocity += 1;
 
         if (y >= SCREEN_HEIGHT - DINO_HEIGHT) {
-            y = SCREEN_HEIGHT - DINO_HEIGHT;
+            y = SCREEN_HEIGHT - DINO_HEIGHT; // DINO_HEIGHT giờ là 100
             velocity = 0;
             isJumping = false;
             jumpCount = 0;
@@ -154,13 +154,11 @@ public:
     }
 
     SDL_Rect getRect() {
-        return {x, y, DINO_WIDTH, DINO_HEIGHT}; // 80x80 for rendering
+        return {x, y, DINO_WIDTH, DINO_HEIGHT}; // 100x100 cho rendering
     }
 
     SDL_Rect getCollisionRect() const {
-        int collisionWidth = 60;
-        int offset = (DINO_WIDTH - collisionWidth) / 2;
-        return {x + offset, y, collisionWidth, DINO_HEIGHT}; // 60x80 for collision
+        return {x, y, DINO_WIDTH, DINO_HEIGHT}; // Bounding box là 100x100
     }
 
     SDL_Texture* getCurrentTexture() {
@@ -245,7 +243,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("Dino Game",
+    SDL_Window* window = SDL_CreateWindow("Vegito in the Prairie",
                                         SDL_WINDOWPOS_UNDEFINED,
                                         SDL_WINDOWPOS_UNDEFINED,
                                         SCREEN_WIDTH,
