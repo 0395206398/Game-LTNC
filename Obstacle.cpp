27a395e1829db1d14b23c463b85be9bf10676681
Ngocduc01 <ducstd1@gmail.com>
2025-03-25 09:24:47 +0700

    Create Obstacle.cpp
    code chướng ngại vật dưới đất

---
+++ Obstacle.cpp
@@ -0,0 +1,22 @@
+#include "Obstacle.h"
+#include "Constants.h"
+
+Obstacle::Obstacle(SDL_Texture* tex) {
+    x = SCREEN_WIDTH;
+    y = SCREEN_HEIGHT - OBSTACLE_HEIGHT;
+    speed = 7.0f;
+    passed = false;
+    texture = tex;
+}
+
+void Obstacle::update() {
+    x -= static_cast<int>(speed);
+}
+
+SDL_Rect Obstacle::getRect() const {
+    return {x, y, OBSTACLE_WIDTH, OBSTACLE_HEIGHT};
+}
+
+bool Obstacle::isOffScreen() const {
+    return x < -OBSTACLE_WIDTH;
+}
