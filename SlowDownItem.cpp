252ef2afb04cbccde42ed5b673963e4fe912fbe1
Ngocduc01 <ducstd1@gmail.com>
2025-03-25 09:25:40 +0700

    Create SlowDownItem.cpp
    code item giảm tốc

---
+++ SlowDownItem.cpp
@@ -0,0 +1,21 @@
+#include "SlowDownItem.h"
+#include "Constants.h"
+
+SlowDownItem::SlowDownItem(SDL_Texture* tex) {
+    x = SCREEN_WIDTH;
+    y = SCREEN_HEIGHT - ITEM_HEIGHT - 20;
+    speed = 7.0f;
+    texture = tex;
+}
+
+void SlowDownItem::update() {
+    x -= static_cast<int>(speed);
+}
+
+SDL_Rect SlowDownItem::getRect() const {
+    return {x, y, ITEM_WIDTH, ITEM_HEIGHT};
+}
+
+bool SlowDownItem::isOffScreen() const {
+    return x < -ITEM_WIDTH;
+}
