c18eba4bd862d10dd3e3e12127760b66f01b342e
Ngocduc01 <ducstd1@gmail.com>
2025-03-26 20:54:58 +0700

    Update Dino.cpp
    thêm thuộc tính bất tử

--- Dino.cpp
+++ Dino.cpp
@@ -14,6 +14,8 @@ Dino::Dino(SDL_Texture* tex1, SDL_Texture* tex2, SDL_Texture* jumpTex) {
     currentRunFrame = false;
     speedMultiplier = 1.0f;
     speedEffectEndTime = 0;
+    isInvincible = false;       // Khởi tạo bất tử
+    invincibilityEndTime = 0;   // Khởi tạo thời gian bất tử
 }
 
 void Dino::reset(SDL_Texture* tex1, SDL_Texture* tex2, SDL_Texture* jumpTex) {
@@ -29,6 +31,8 @@ void Dino::reset(SDL_Texture* tex1, SDL_Texture* tex2, SDL_Texture* jumpTex) {
     currentRunFrame = false;
     speedMultiplier = 1.0f;
     speedEffectEndTime = 0;
+    isInvincible = false;       // Reset bất tử
+    invincibilityEndTime = 0;   // Reset thời gian bất tử
 }
 
 void Dino::jump() {
@@ -42,6 +46,18 @@ void Dino::jump() {
 void Dino::applySpeedEffect(float multiplier) {
     speedMultiplier = multiplier;
     speedEffectEndTime = SDL_GetTicks() + SPEED_EFFECT_DURATION;
+}
+
+void Dino::applyInvincibility() { // Triển khai phương thức bất tử
+    isInvincible = true;
+    invincibilityEndTime = SDL_GetTicks() + 5000; // 5 giây
+}
+
+bool Dino::checkInvincibility() const { // Kiểm tra bất tử
+    if (isInvincible && SDL_GetTicks() > invincibilityEndTime) {
+        return false; // Hết thời gian bất tử
+    }
+    return isInvincible;
 }
 
 void Dino::update() {
