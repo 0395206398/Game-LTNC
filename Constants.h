39e0f81ddb602b4b9ec5577bce1663d5891052c9
Ngocduc01 <ducstd1@gmail.com>
2025-03-26 20:54:42 +0700

    Update Constants.h
    chỉnh sửa lại kích thước của chướng ngại vật bay sao cho hợp lí

--- Constants.h
+++ Constants.h
@@ -7,12 +7,12 @@ const int DINO_WIDTH = 100;
 const int DINO_HEIGHT = 100;
 const int OBSTACLE_WIDTH = 40;
 const int OBSTACLE_HEIGHT = 80;
-const int FLYING_OBSTACLE_WIDTH = 80;
-const int FLYING_OBSTACLE_HEIGHT = 80;
+const int FLYING_OBSTACLE_WIDTH = 64;
+const int FLYING_OBSTACLE_HEIGHT = 64;
 const int ITEM_WIDTH = 40;
 const int ITEM_HEIGHT = 40;
 const Uint32 SPEED_EFFECT_DURATION = 5000; // 5 seconds for speed effect
-
+const int GROUND_HEIGHT = 0;
 enum GameState {
     START,
     PLAYING,
